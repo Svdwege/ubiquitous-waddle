9a173cea816a95b758ecc97c867328288165e40b
jos <jos.onokiewicz@han.nl>
2020-08-19 13:55:49 +0200

    Update to C99

--- voidPointers/main.c
+++ voidPointers/main.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-   int iVar = 1;
-   double dVar = 2.2;
-   char cVar = 'c';
    void *pGeneric = NULL;
 
+   int iVar = 1;
    pGeneric = &iVar;
    printf(" pGeneric = %p points to %d\n", pGeneric, *(int *)pGeneric);
 
+   double dVar = 2.2;
    pGeneric = &dVar;
    printf(" pGeneric = %p points to %lf\n", pGeneric, *(double *)pGeneric);
 
+   char cVar = 'c';
    pGeneric = &cVar;
    printf(" pGeneric = %p points to %c\n", pGeneric, *(char *)pGeneric);
 
