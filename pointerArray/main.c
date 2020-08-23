03e2fc64250e5454f10d6e9fdc3f3296c5e90b6b
jos <jos.onokiewicz@han.nl>
2020-08-23 14:45:11 +0200

    Improve code

--- pointerArray/main.c
+++ pointerArray/main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
-#define SIZE 4
+#define SIZE 5
 
 int main(void)
 {
    /* Every string should be considered a pointer to the string */
-   const char *stringData[SIZE] = {"Hello", "world", "of", "pointers"};
+   const char *stringData[SIZE] = {"Hello", "world", "of", "pointers",
+                                   "in arrays"};
 
    for (int i = 0; i < SIZE; i++)
    {
-      printf("stringData[%d] = \"%s\"\n", i, stringData[i]);
+      printf("stringData[%d] = %p  points to \"%s\"\n", i, stringData[i],
+             stringData[i]);
    }
    puts("\n");
 
    /* Only printing the first character of every string */
+   puts("First character of every string:");
    for (int i = 0; i < SIZE; i++)
    {
       printf("stringData[%d][0] = '%c'\n", i, stringData[i][0]);
    }
-   puts("\n");
 
    return 0;
 }
