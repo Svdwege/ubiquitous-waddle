40c15eb9a36ce4aed86e476e027e971fee22a6e3
jos <jos.onokiewicz@han.nl>
2020-12-16 09:35:44 +0100

    Improve code,  add formulas to README.md

--- functions/testUnity/testMain.c
+++ functions/testUnity/testMain.c
@@ -1,12 +1,18 @@
 /* Unit tests: using Unity unit test framework
  *
  * Test code should be complemented with more tests.
+ * Before solving bugs in the code of the functions,
+ * check always if the the unit test code is correct.
  */
 
 #include "functions.h"
 #include "unity.h"
 
 #include <stdio.h>
+
+#undef TEST_ASSERT_MESSAGE
+#define TEST_ASSERT_MESSAGE(condition)                                         \
+   UNITY_TEST_ASSERT(condition, __LINE__, #condition " is not ok.");
 
 const float DELTA = 0.001f;
 
@@ -18,16 +24,20 @@ void test_findMinMax(void)
       float data1[1] = {-1.0};
       minmax_t result = findMinMax(data1, 1);
 
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, -1.0, result.min, "test 1");
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, -1.0, result.max, "test 2");
+      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, -1.0, result.min,
+                                       "Minimum value not ok");
+      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, -1.0, result.max,
+                                       "Maximum value not ok");
    }
 
    {
       float data2[5] = {-1.0, 0.0, 1.0, 2.0, 3.0};
       minmax_t result = findMinMax(data2, 5);
 
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, -1.0, result.min, "test 3");
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 3.0, result.max, "test 4");
+      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, -1.0, result.min,
+                                       "Minimum value not ok");
+      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 3.0, result.max,
+                                       "Maximum value not ok");
    }
 }
 
@@ -39,14 +49,14 @@ void test_averageData(void)
       float data1[1] = {1.0};
       float result = averageData(data1, 1);
 
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 1.0, result, "test 1");
+      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 1.0, result, "Average not ok");
    }
 
    {
       float data2[5] = {1.0, 1.0, 1.0, 1.0, 1.0};
       float result = averageData(data2, 5);
 
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 1.0, result, "test 2");
+      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 1.0, result, "Average not ok");
    }
 }
 
@@ -60,11 +70,9 @@ void test_meanNormalisation(void)
       meanNormalisation(data1, 5);
       minmax_t result = findMinMax(data1, 5);
 
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, -1.0, result.min, "test 1");
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 1.0, result.max, "test 2");
-
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 0.0, averageData(data1, 5),
-                                       "test 3");
+      TEST_ASSERT_MESSAGE(result.min >= 1.0);
+      TEST_ASSERT_MESSAGE(result.max <= 1.0);
+      TEST_ASSERT_FLOAT_WITHIN(DELTA, 0.0, averageData(data1, 5));
    }
 
    {
@@ -73,11 +81,9 @@ void test_meanNormalisation(void)
       meanNormalisation(data2, 5);
       minmax_t result = findMinMax(data2, 5);
 
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, -1.0, result.min, "test 4");
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 1.0, result.max, "test 5");
-
-      TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, 0.0, averageData(data2, 5),
-                                       "test 6");
+      TEST_ASSERT(result.min >= -1.0);
+      TEST_ASSERT(result.max <= 1.0);
+      TEST_ASSERT_FLOAT_WITHIN(DELTA, 0.0, averageData(data2, 5));
    }
 }
 
@@ -93,7 +99,7 @@ void test_minmaxScaling(void)
 
    minmaxScaling(data1, 4, min, max);
    TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, min, findMinMax(data1, 4).min,
-                                    "test 1");
+                                    "Minimum value not ok");
 
    min = -2.0;
    max = 4.0;
@@ -101,15 +107,15 @@ void test_minmaxScaling(void)
 
    minmaxScaling(data1, 4, min, max);
    TEST_ASSERT_FLOAT_WITHIN_MESSAGE(DELTA, max, findMinMax(data1, 4).max,
-                                    "test 2");
+                                    "Maximum value not ok");
 }
 
 int main(void)
 {
    UNITY_BEGIN();
 
-   printf("== Unity unit tests 'functions' ==\n\n");
-   
+   printf("=== Unity unit tests 'functions' module ===\n\n");
+
    RUN_TEST(test_findMinMax);
    RUN_TEST(test_averageData);
    RUN_TEST(test_meanNormalisation);
